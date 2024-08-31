08f4be36c70e1e21786fe822786ce0d80f600577
ibrahim-ibrahim <folahanmi0001@gmail.com>
2024-08-31 02:26:30 +0100

    feat: check faucet balance before processing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use solana_client::rpc_client::RpcClient;
+use solana_sdk::native_token::lamports_to_sol;
 use solana_sdk::{
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
     signature::{Keypair, Signer},
+    signer::keypair::{read_keypair_file, write_keypair_file},
     system_program,
     transaction::Transaction,
-    signer::keypair::{read_keypair_file, write_keypair_file},
 };
 use std::str::FromStr;
-use solana_sdk::native_token::lamports_to_sol;
 
 fn main() {
     let rpc_url = "https://api.devnet.solana.com".to_string();
@@ -17,10 +17,14 @@ fn main() {
     let raw_program_id = "5gpW17UnnPPzgdhdoHBBJM75fmZaCvX14DjvtxqXsqCY";
     let program_id = Pubkey::from_str(raw_program_id).expect("Failed to parse program ID");
 
-    let faucet_keypair = read_keypair_file("faucet_keypair.json").expect("Failed to read faucet keypair");
+    let faucet_keypair =
+        read_keypair_file("faucet_keypair.json").expect("Failed to read faucet keypair");
     let user_keypair = generate_and_save_keypair();
 
     println!("User keypair: {}", user_keypair.pubkey());
+
+    // Check faucet balance before processing
+    check_faucet_balance(&client, &faucet_keypair.pubkey(), 1).expect("Faucet balance is too low");
 
     let instruction = Instruction::new_with_bincode(
         program_id,
@@ -44,12 +48,23 @@ fn main() {
     println!("Transaction signature: {}", signature);
 
     let faucet_balance = client.get_balance(&faucet_keypair.pubkey()).unwrap();
-    println!("Faucet current balance: {} lamports ({} SOL)", faucet_balance, lamports_to_sol(faucet_balance));
+    println!(
+        "Faucet current balance: {} lamports ({} SOL)",
+        faucet_balance,
+        lamports_to_sol(faucet_balance)
+    );
 
     let user_balance = client.get_balance(&user_keypair.pubkey()).unwrap();
-    println!("User current balance: {} lamports ({} SOL)", user_balance, lamports_to_sol(user_balance));
+    println!(
+        "User current balance: {} lamports ({} SOL)",
+        user_balance,
+        lamports_to_sol(user_balance)
+    );
 
-    let transaction_url = format!("https://explorer.solana.com/tx/{}?cluster=devnet", signature);
+    let transaction_url = format!(
+        "https://explorer.solana.com/tx/{}?cluster=devnet",
+        signature
+    );
     println!("Transaction URL: {}", transaction_url);
 }
 
@@ -68,4 +83,29 @@ fn generate_and_save_keypair() -> Keypair {
         println!("Keypair file already exists");
         read_keypair_file(file_path).expect("Failed to read keypair file")
     }
+}
+
+// Check the SOL balance in the faucet account
+fn check_faucet_balance(
+    client: &RpcClient,
+    faucet_pubkey: &Pubkey,
+    min_balance: u32,
+) -> Result<(), String> {
+    let faucet_balance = client.get_balance(&faucet_pubkey).unwrap_or(0);
+    let balance_in_sol = lamports_to_sol(faucet_balance);
+
+    if balance_in_sol < min_balance as f64 {
+        return Err(format!(
+            "Faucet balance is too low. Current balance: {} SOL, Minimum required: {} SOL",
+            balance_in_sol, min_balance,
+        ));
+    }
+
+    println!(
+        "Faucet current balance: {} lamports ({} SOL)",
+        faucet_balance,
+        lamports_to_sol(faucet_balance)
+    );
+
+    Ok(())
 }
